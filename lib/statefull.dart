847240c183811acabb6de13f1bd831249920bbc6
babyboy <bugs.bunny.shah@gmail.com>
2020-02-04 11:55:56 -0600

    third_commit

--- lib/statefull.dart
+++ lib/statefull.dart
@@ -7,23 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: RandomWords(), appBar: AppBar(title: Text('Welcome to Flutter'),), 
-                           body: Center( child: RandomWords())            
-            )
+    return Scaffold(
+      appBar: AppBar(title: Text('Startup Name Generator')),
+      body: RandomWords()
     );
   }
 }
